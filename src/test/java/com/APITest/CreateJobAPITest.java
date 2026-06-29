8910949d19c8529c9386b3a6082db0bb83f7384d
roshan <roshan.shresthama@gmail.com>
2026-06-29 22:17:46 +0530

    Updated the framework to JAVA16
    Used the records features  of JAVA 16 converted the POJO classes in
    record and stored it in com.api.POJO

--- src/test/java/com/APITest/CreateJobAPITest.java
+++ src/test/java/com/APITest/CreateJobAPITest.java
@@ -2,6 +2,10 @@ package com.APITest;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import com.api.constant.Role;
@@ -12,6 +16,8 @@ import com.api.pojo.FdCustomerData;
 import com.api.pojo.Problems;
 import com.api.utils.SpecUtil;
 
+import io.restassured.module.jsv.JsonSchemaValidator;
+
 public class CreateJobAPITest {
 	//probel pojo class
 	
@@ -19,24 +25,23 @@ public class CreateJobAPITest {
 	public void createJobTest() {
 		Problems problem1=new Problems(2, "screen issue");
 		//declaration initilaizatio of array type is Problems which we have created the POJO
-		Problems[] problem=new Problems[1];
-		problem[0]=problem1;
+		List<Problems>problem=new ArrayList<Problems>();
+		problem.add(problem1);
 		
-		FDCustomerProduct customerproduct=new FDCustomerProduct("2026-02-01T18:30:00.000Z", "820467366797292", "820467366797292", "820467366797292", "2026-02-01T18:30:00.000Z", 1, 1);
+		FDCustomerProduct customerproduct=new FDCustomerProduct("2026-02-01T18:30:00.000Z", "124467466792792", "124467466792792", "124467466792792", "2026-02-01T18:30:00.000Z", 1, 1);
 		FDCustomerAddess customerAdd=new FDCustomerAddess("helloflAT", "namsate aprt", "gajANAN", "wola", "400708", "560023", "mh", "");
-		FdCustomerData customer= new FdCustomerData("ros", "hos", "1234567891", "", "hos@gmail.com", "");
+		FdCustomerData customer= new FdCustomerData("ros", "hos", "1234967891", "", "hos@gmail.com", "");
 		CreateJobFD createjobfd=new CreateJobFD(0, 2, 1, 1, customer, customerAdd, customerproduct, problem);
 		given()
 		  .spec(SpecUtil.requestWithAuth(Role.FD, createjobfd))
-		  
-  .log().uri()
-  .log().method()
-  .log().headers()
-	.when()
+		 .when()
 	  .post("/job/create")
 	.then()
-	 .log().all()
-	 .statusCode(200);
+	 .spec(SpecUtil.responseSpec_OK())
+	 .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("Response_Schema/CreateJOBFD.json"))
+	 .body("message", Matchers.equalTo("Job created successfully. "))
+	 .body("data.mst_service_location_id", Matchers.equalTo(1))
+	 .body("data.job_number", Matchers.startsWith("JOB_"));
 		
 	}
 
