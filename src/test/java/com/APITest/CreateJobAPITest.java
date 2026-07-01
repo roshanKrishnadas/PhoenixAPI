d1db43864d158133c7d42ce6c4c52511938705d3
roshan <roshan.shresthama@gmail.com>
2026-07-01 21:59:14 +0530

    created the ENUM for removing the hard coding id from create job test and refractor the creatJOBAPI test

--- src/test/java/com/APITest/CreateJobAPITest.java
+++ src/test/java/com/APITest/CreateJobAPITest.java
@@ -8,7 +8,14 @@ import java.util.List;
 import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
+import com.api.constant.MstModelId;
+import com.api.constant.OEM;
+import com.api.constant.Platform;
+import com.api.constant.Problem;
+import com.api.constant.Product;
 import com.api.constant.Role;
+import com.api.constant.ServiceLocation;
+import com.api.constant.Warranty_Status;
 import com.api.request.model.CreateJobFD;
 import com.api.request.model.FDCustomerAddess;
 import com.api.request.model.FDCustomerProduct;
@@ -24,15 +31,15 @@ public class CreateJobAPITest {
 	
 	@Test
 	public void createJobTest() {
-		Problems problem1=new Problems(2, "screen issue");
+		Problems problem1=new Problems(Problem.Poor_battery_life.getCode(), "screen issue");
 		//declaration initilaizatio of array type is Problems which we have created the POJO
 		List<Problems>problem=new ArrayList<Problems>();
 		problem.add(problem1);
 		
-		FDCustomerProduct customerproduct=new FDCustomerProduct(DateTimeUtil.getTimeWithDaysAgo(01), "125467466792792", "125467466792792", "125467466792792", DateTimeUtil.getTimeWithDaysAgo(01), 1, 1);
+		FDCustomerProduct customerproduct=new FDCustomerProduct(DateTimeUtil.getTimeWithDaysAgo(01), "126467466792792", "126467466792792", "126467466792792", DateTimeUtil.getTimeWithDaysAgo(01), Product.NEXUS_2.getCode(), MstModelId.Nexus_2blue.getMstModelId());
 		FDCustomerAddess customerAdd=new FDCustomerAddess("helloflAT", "namsate aprt", "gajANAN", "wola", "400708", "560023", "mh", "");
 		FdCustomerData customer= new FdCustomerData("ros", "hos", "1234967891", "", "hos@gmail.com", "");
-		CreateJobFD createjobfd=new CreateJobFD(0, 2, 1, 1, customer, customerAdd, customerproduct, problem);
+		CreateJobFD createjobfd=new CreateJobFD(ServiceLocation.SERVICE_LOCATION_A.getCode(), Platform.Front_Desk.getCode(), Warranty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(), customer, customerAdd, customerproduct, problem);
 		given()
 		  .spec(SpecUtil.requestWithAuth(Role.FD, createjobfd))
 		 .when()
