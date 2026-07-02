a53ddf4fcfe22404778ce48cfd170d16719b4795
roshan <roshan.shresthama@gmail.com>
2026-07-02 21:53:02 +0530

    just refractor the demo to check theci.yml repo should execute on push event

--- src/test/java/com/api/utils/Demo.java
+++ src/test/java/com/api/utils/Demo.java
@@ -1,10 +1,18 @@
 package com.api.utils;
 
 public class Demo {
-
+ int a;
+ int b;
+ public Demo(int a, int b) {
+	 this.a=a;
+	 this.b=b;
+ }
+ 
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
-
+		
+      
 	}
+	
 
 }
