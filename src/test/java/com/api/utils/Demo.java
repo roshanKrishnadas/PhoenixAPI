731fade72ac5b1c2f899ecdef0dfd58d55115ddd
roshan <roshan.shresthama@gmail.com>
2026-07-03 07:21:03 +0530

    added c varaibles

--- src/test/java/com/api/utils/Demo.java
+++ src/test/java/com/api/utils/Demo.java
@@ -3,9 +3,11 @@ package com.api.utils;
 public class Demo {
  int a;
  int b;
- public Demo(int a, int b) {
+ int c;
+ public Demo(int a, int b,int c) {
 	 this.a=a;
 	 this.b=b;
+	 this.c=c;
  }
  public void setA(int a) {
 	 a=3;
@@ -13,17 +15,23 @@ public class Demo {
  public void setB(int b) {
 	 b=3;
  }
+ public void setC(int c) {
+	 c=3;
+ }
  public int getIntA() {
 	 return a;
  }
  public int getIntB() {
 	 return b;
  }
+ public int getIntC() {
+	 return c;
+ }
  
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
-		Demo d =new Demo(1, 1);
-		System.out.println(d.a +","+d.b);
+		Demo d =new Demo(1, 1,1);
+		System.out.println(d.a +","+ d.b +"," + d.c);
 		
       
 	}
