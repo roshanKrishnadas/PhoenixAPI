110c95430394623730978da012948557ce1a5f55
roshan <roshan.shresthama@gmail.com>
2026-07-03 07:03:04 +0530

    added the static varables in demo

--- src/test/java/com/api/utils/Demo.java
+++ src/test/java/com/api/utils/Demo.java
@@ -1,9 +1,12 @@
 package com.api.utils;
 
 public class Demo {
-
+	
+static int a=1;
+static int b=2;
 	public static void main(String[] args) {
 		System.out.println(System.getProperty("user.dir"));
+		System.out.println(a+b);
 
 	}
 
