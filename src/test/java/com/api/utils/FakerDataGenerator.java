d376e2241a8d9efb443f9d436cd6d55fcedc8773
roshan <roshan.shresthama@gmail.com>
2026-07-15 07:26:16 +0530

    fixed the problem Id issued by clloecting the valid ids and storing into the []from this fixed array we will pick the ids

--- src/test/java/com/api/utils/FakerDataGenerator.java
+++ src/test/java/com/api/utils/FakerDataGenerator.java
@@ -24,6 +24,7 @@ public class FakerDataGenerator {
 	private final static int MST_OEM_ID=1;
 	private final static int PRODUCT_ID= 1;
 	private final static int  MST_MODEL_ID=1;
+	private final static int VALIDPROBLEMIDs[]={1,2,3,4,5,6,7,8,9,10,11,12,15,16,17,19,20,22,24,26,27,28,29};
 	private  FakerDataGenerator() {
 	}
 		public static CreateJobFD generateFakerCreateJobData() {
@@ -52,8 +53,8 @@ public class FakerDataGenerator {
 		private static List<Problems> genearteListOfProblem() {
 			String fakeRemark=FAKER.lorem().sentence(2);
 			
-			int problemID=RANDOM.nextInt(27)+1;
-			Problems problem=new Problems(problemID, fakeRemark);
+			int problemID=RANDOM.nextInt(VALIDPROBLEMIDs.length);
+			Problems problem=new Problems(VALIDPROBLEMIDs[problemID], fakeRemark);
 			
 			List<Problems> problemList=new ArrayList<Problems>();
 			problemList.add(problem);
